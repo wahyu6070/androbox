3150401ce8675edc69eafbec2d8bd8c81c3df746
wahyu6070 <gwtsendangrejo6070@gmail.com>
2021-02-04 18:41:50 +0700

    initial

---
+++ androbox.go
@@ -0,0 +1,23 @@
+// Androbox by wahyu kurniawan (wahyu6070)
+// written the first time from zero = 04-02-2021 01.44 WIB FROM asus zenfone max pro m1 (X00T) (android mobilr)
+// 
+// latest update
+// version=1.0
+// version=code=1
+
+package main
+import(
+"fmt"
+"os/exec"
+)
+func main(){
+	for true {
+		exec.Command("clear")
+		fmt.Println("      Androbox")
+		fmt.Println(" ")
+		break
+		
+		}
+	
+	
+	}
