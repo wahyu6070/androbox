836f4fdf1e484310dabe6b1e1a6632411e39a0bf
wahyu6070 <gwtsendangrejo6070@gmail.com>
2021-02-09 16:19:45 +0700

    added device info

--- androbox.go
+++ androbox.go
@@ -1,5 +1,5 @@
 // Androbox by wahyu kurniawan (wahyu6070)
-// written the first time from zero = 04-02-2021 01.44 WIB FROM asus zenfone max pro m1 (X00T) (android mobilr)
+// written the first time from zero = 04-02-2021 01.44 WIB FROM asus zenfone max pro m1 (X00T) (android mobile)
 // 
 // latest update
 // version=1.0
@@ -39,30 +39,87 @@ func CallClear() {
     }
 }
 
+func boost(){
+	fmt.Println("Boost CPU")
+	
+	
+	
+	}
 
+func device_info(){
+	stopp:
+	for true {
+		CallClear()
+		fmt.Println("             Device Info ")
+		fmt.Println("  ")
+		if _, err := os.Stat("/data/adb/busybox"); err == nil {
+    	fmt.Printf("- Busybox Installed\n");  
+  		} else {
+    	fmt.Printf("File does not exist\n");  
+    	}
+    	
+    	fmt.Println(" ")
+    	fmt.Printf("1.Back\n")
+    	fmt.Println(" ")
+    	fmt.Printf(" Select Menu : ")
+    	var input1 int
+		fmt.Scanln(&input1)
+		switch input1 {
+			case 1:
+			break stopp
+			default:
+			break stopp
+			}
+	
+	}
+	}
+func about(){
+	stopp:
+	for true {
+		CallClear()
+		fmt.Println("             About ")
+		fmt.Println("  ")
+		fmt.Println("Androbox 1.0 (1) Beta")
+		fmt.Println("Androbox Lincense GPL2")
+    	
+    	fmt.Println(" ")
+    	fmt.Printf("1.Back\n")
+    	fmt.Println(" ")
+    	fmt.Printf(" Select Menu : ")
+    	var input1 int
+		fmt.Scanln(&input1)
+		switch input1 {
+			case 1:
+			break stopp
+			default:
+			break stopp
+			}
+	
+	}
+	
+	}
 func main(){
 	berhentii:
 	for true {
 		CallClear()
 		fmt.Println("      Androbox")
 		fmt.Println(" ")
-		fmt.Println("1.Boost")
-		fmt.Println("2.About")
-		fmt.Println("3.Exit")
+		fmt.Println(" 1.Boost")
+		fmt.Println(" 2.Device Info")
+		fmt.Println(" 3.About")
+		fmt.Println(" 4.Exit")
 		fmt.Println(" ")
-		fmt.Println("Select Menu : \n")
+		fmt.Printf("  Select Menu : ")
 		var inputterm int
 		fmt.Scanln(&inputterm)
 		switch inputterm {
 			case 1:
-			pwdd := exec.Command("sh", "asw.sh")
-			pwdd.Stdout = os.Stdout
-			pwdd.Run()
-			fmt.Println("lol")
-			time.Sleep(3 * time.Second)
+			boost()
 			case 2:
-			fmt.Println("lol")
+			device_info()
 			case 3:
+			about()
+			case 4:
 			break berhentii
 			default:
 			fmt.Println("!!! Please select menu")
